0ec24801ea6da2234f5ec14b6186f2baeb10cb31
gitdog01 <hostdog01@naver.com>
2023-12-12 22:29:59 +0900

    dumy data

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const router = express.Router();
 
-router.get("/callback", (req, res) => {
-  const result = axios({
+router.get("/callback", async (req, res) => {
+  const result = await axios({
     method: "post",
     url: `https://github.com/login/oauth/access_token`,
     headers: {
@@ -17,7 +17,37 @@ router.get("/callback", (req, res) => {
     },
   });
 
+  if (result.data.error) {
+    console.log(result.data.error);
+    res.json(false);
+  }
   res.json(true);
+});
+
+router.get("data", async (req, res) => {
+  const starCount = 55; // number
+  const followerCount = 123; // number
+  const followingCount = 223; // number
+  const mostUsedLanguage = ["JavaScript", "TypeScript", "Python"]; // string[]
+  const moreThan = "high"; // high, middle, low
+  const commitCount = 1234; // number
+  const commitDate = "월요일"; // 월요일, 화요일, 수요일, 목요일, 금요일, 토요일, 일요일
+  const mostCommunication = {
+    // 여기 줄때 github profile 주소도 주면 좋지 않을까 ?
+    name: "bsy1141", // string
+    image: "https://avatars.githubusercontent.com/u/60652298?v=4", // string
+  };
+
+  res.json({
+    starCount,
+    followerCount,
+    followingCount,
+    mostUsedLanguage,
+    moreThan,
+    commitCount,
+    commitDate,
+    mostCommunication,
+  });
 });
 
 export default router;
