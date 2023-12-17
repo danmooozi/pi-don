3cd7e475650f66b1a0838f8284419b7b79fb38d5
rolled-potatoes <keep4404@gmail.com>
2023-12-17 13:43:39 +0900

    fix: git auth code 로직 변경

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
-import express from "express";
-import v1Router from "./v1";
-import v2Router from "./v2";
+import express from 'express';
+import v1Router from './v1';
+import v2Router from './v2';
 
 const router = express.Router();
 
-router.use("/v1", v1Router);
-router.use("/v2", v2Router);
+router.use('/v1', v1Router);
+router.use('/v2', v2Router);
 
 export default router;
